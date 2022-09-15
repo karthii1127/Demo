31ea4a6087f1676f75bd3f9886a10d9cc2863436
karthii <karthikraj1439288@gmail.com>
2022-09-15 22:20:18 +0530

    1st Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const parser = require("body-parser");
+const app = express();
+// app.use(parser.json());
+app.use(parser.urlencoded({extended:true}))
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+    console.log(__dirname);
+    // res.send("Hello world i am karthik");
+})
+app.post('/',(req, res)=>{
+    console.log("Output");
+    var height = Number(req.body.height);
+    var weight = Number(req.body.weight);
+    res.send("Your Bmi is "+(weight/(height*height)));
+    // console.log(Number(req.body.height) +Number(req.body.weight));
+    // res.send("Value "+ (Number(req.body.height) +Number(req.body.weight)))
+})
+ var port = 3000;
+
+app.listen(port,()=>{
+    console.log("Listening in Port "+port);
+})
